fa77cf03c93d1976a943b8b19f1acb8e7f3dd1f2
Selmir Hajdarevic <selmirhajdarevic@tutanota.com>
2018-09-18 15:51:29 +0200

    Improve layout and finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,45 @@
 class UsersController < ApplicationController
-  def new
+
+  def index
+    @users = User.all
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    # params[:id] is the string "1",
+    # but find is smart enough to convert this to an integer.
+    @user = User.find(params[:id])
+
+    # byebug
+    # debugger
+
+    # whenever you’re confused about something in a Rails application,
+    # it’s a good practice to put debugger close to the code you think
+    # might be causing the trouble.
+  end
+
+  private
+
+  # permit user's params (name, email, password, password confirmation...
+  # but no others
+  # this code returns a version of the params hash with only the permitted
+  # attributes (while raising an error if the :user attribute is missing).
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+      :password_confirmation)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
+
+  root 'static_pages#home'
   get 'static_pages/home'
   get  'static_pages/about'
   get 'static_pages/help'
   get 'static_pages/contact'
+  # get  '/contact', to: 'static_pages#contact'
+  # get  'users/new'
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
 
-  root 'static_pages#home'
+
+  resources :users
+
 end
